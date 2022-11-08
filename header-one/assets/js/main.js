9ca8eee93ffb3438a3cd9920f72dd717502bef6b
else-dev <71620505+Faisal611@users.noreply.github.com>
2022-11-08 16:05:22 +0600

    public

--- header-one/assets/js/main.js
+++ header-one/assets/js/main.js
@@ -1,13 +1,12 @@
 (function ($) {
     $(document).ready (function (){
-        "use strict";
 
-        $('.menu-toggle').click(function (){
-            $('.site-nav').toggleClass('site-nav--open');
-            $(this).toggleClass('open');
+        "use strict"
+        $('.menu-toggle').click(function () {
+            $('.site-nav').toggleClass('site-nav--open',500);
+            $(this).toggleClass('open')
 
         })
 
-
     });
-})(jQuery)
+})(jQuery);
